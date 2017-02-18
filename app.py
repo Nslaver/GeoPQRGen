560b1bd40f21c76ee4bc08a9e85f6bc0f08e6e1b
Hans Burbano <hburbano@bbox.co>
2017-02-18 18:51:08 -0500

    Update git to ignore images and IDE files

--- app.py
+++ app.py
@@ -2,31 +2,34 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
 import qrcode
+import qrcode.image.svg
+import svgwrite
+
+method = 'test'
+
+if method == 'basic':
+    # Simple factory, just a set of rects.
+    factory = qrcode.image.svg.SvgImage
+elif method == 'fragment':
+    # Fragment factory (also just a set of rects)
+    factory = qrcode.image.svg.SvgFragmentImage
+else:
+    # Combined path factory, fixes white space that may occur when zooming
+    factory = qrcode.image.svg.SvgPathImage
+
 qr = qrcode.QRCode(
     version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=20,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    image_factory=factory,
+    box_size=10,
     border=4,
 )
 
-qr.add_data('ASDFASDFASDFASDFtest')
+qr.add_data('dfasdfasdf')
 qr.make(fit=True)
+img = qr.make_image()
+dwg = svgwrite.Drawing(filename='test.svg')
+dwg.
 
-fnt = ImageFont.truetype("FreeMono.ttf", 16)
-base = qr.make_image()
 
-txt = Image.new('RGB', base.size, (255,255,255,0))
-
-# get a font
-fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
-# get a drawing context
-d = ImageDraw.Draw(txt)
-
-# draw text, half opacity
-d.text((10,10), "Hello", font=fnt, fill=(255,255,255,128))
-# draw text, full opacity
-d.text((10,60), "World", font=fnt, fill=(255,255,255,255))
-
-out = Image.composite(base, txt, 1)
-
-out.show()
+dwg.save()
