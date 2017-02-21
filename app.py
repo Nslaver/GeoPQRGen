5f40f2e6d37c3d25c1d2e9f37e2551a6fc0bc292
Hans Burbano <hburbano@bbox.co>
2017-02-20 23:18:43 -0500

    Add simple decoration

--- app.py
+++ app.py
@@ -19,30 +19,30 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
 
 method = 'test'
 i = 1
-l = 100
-size = 40
-font = ImageFont.truetype("arial.ttf", size)
+l = 10
+fontSize = 40
+font = ImageFont.truetype("arial.ttf", fontSize)
 printProgressBar(i, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 for x in range(0, l):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
-        border=4,
     )
 
     i += 1
     printProgressBar(i, l, prefix='Progress:', suffix='Complete', length=50)
     newFile = '{:04d}'.format(x)+'.png'
-    qr.add_data('CasterCos'+str(x))
+    qr.add_data('CasterCos'+'{:04d}'.format(x))
     qr.make(fit=True)
     qrImg = qr.make_image().get_image()
     newImg = Image.new('RGB',(qrImg.width,qrImg.width*2),(255, 255, 255))
     newImg.paste(qrImg, (0, 10, qrImg.width, 10+qrImg.width))
 
-    toText = ImageDraw.Draw(newImg)
-    toText.text((5, qrImg.width+(size)), newFile, (0, 0, 0), font=font)
-    toText.text((5, qrImg.width+(size*2)), "Sample Text 1", (0, 0, 0), font=font)
-    toText.text((5, qrImg.width+(size*3)), "Sample Text 2", (0, 0, 0), font=font)
+    canvas = ImageDraw.Draw(newImg)
+    padd = 5
+    canvas.rectangle([padd,padd,newImg.width-padd,newImg.height-padd],outline=(0,0,0))
+    canvas.text((5, qrImg.width+(fontSize)), newFile, (0, 0, 0), font=font)
+    canvas.text((5, qrImg.width+(fontSize*2)), "Sample Text 1", (0, 0, 0), font=font)
+    canvas.text((5, qrImg.width+(fontSize*3)), "Sample Text 2", (0, 0, 0), font=font)
 
     newImg.save(newFile)
